7c008291d625f98c4fb3b73da486db970aafebc8
willy <reyno.willy@gmail.com>
2015-03-09 16:31:48 +0100

    Update functions

--- wp-content/themes/themename/functions.php
+++ wp-content/themes/themename/functions.php
@@ -1,2 +1,7 @@
 <?php
 require_once('functions/wp_bootstrap_navwalker.php');
+
+function register_my_menu() {
+    register_nav_menu('principal',__( 'Menu Principal' ));
+}
+add_action( 'init', 'register_my_menu' );
